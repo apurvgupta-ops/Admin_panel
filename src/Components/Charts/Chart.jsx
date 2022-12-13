08a42a9264f5c88ae9229726b73939ebe9ea754a
Apurv gupta <apurvgupta.124@gmail.com>
2022-12-13 12:08:13 +0530

    Single user

--- src/Components/Charts/Chart.jsx
+++ src/Components/Charts/Chart.jsx
@@ -20,11 +20,11 @@ const data = [
   { name: "June", Total: 200 },
 ];
 
-const Chart = () => {
+const Chart = ({ title, aspect }) => {
   return (
     <div className="chart">
-      <div className="title">Last 6 Months (Revenue)</div>
-      <ResponsiveContainer width="100%" aspect={2 / 1}>
+      <div className="title">{title}</div>
+      <ResponsiveContainer width="100%" aspect={aspect}>
         <AreaChart
           width={730}
           height={250}
--- src/Pages/SingleUser/SingleUser.jsx
+++ src/Pages/SingleUser/SingleUser.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import "./singleUser.scss";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Navbar from "../../Components/Navbar/Navbar";
+import SingleUserDetail from "../../Components/Single User Detail/SingleUserDetails";
 
 const SingleUser = () => {
-  return (
-    <div>SingleUser</div>
-  )
-}
+  return <SingleUserDetail />;
+};
 
-export default SingleUser
+export default SingleUser;
